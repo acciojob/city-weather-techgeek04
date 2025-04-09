9a1e38929a72194bfb1ee70e7f1905572af0c076
techgeek04 <155185231+techgeek04@users.noreply.github.com>
2025-04-09 14:51:56 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,50 @@
+import React, { useState } from "react";
 
-import React from "react";
-import './../styles/App.css';
+const API_KEY = "e467712b257e418838be97cc881a71de";
 
-const App = () => {
+function App() {
+  const [query, setQuery] = useState("");
+  const [weather, setWeather] = useState(null);
+
+  const search =  (e) => {
+    if (e.key === "Enter") {
+      fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${API_KEY}`
+      ).then((res)=>res.json()).then((resData)=>setWeather(resData))
+      
+      setQuery("");
+    }
+  };
+
+  const kelvinToFahrenheit = (k) => ((k - 273.15) * 9) / 5 + 32;
+
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div className="app">
+      <input
+        type="text"
+        className="search"
+        placeholder="Enter a city"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        onKeyPress={search}
+      />
+      {weather && (
+        <div className="weather">
+          <div className="city">{weather.name}</div>
+          <div className="temperature">
+            {Math.round(kelvinToFahrenheit(weather.main.temp))}°F
+          </div>
+          <div className="description">{weather.weather[0].description}</div>
+          <div className="icon">
+            <img
+              src={`http://openweathermap.org/img/w/${weather.weather[0].icon}.png`}
+              alt={weather.weather[0].description}
+            />
+          </div>
+        </div>
+      )}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
